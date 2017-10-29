43ed0e801c8c29b8209e8f6cf0665b872a4e15f9
abbireisig <31539009+abbireisig@users.noreply.github.com>
2017-10-29 19:45:26 -0400

    research

--- app/src/main/java/com/example/abbi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/abbi/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button bt1;
 
-    @Override
+    //@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
